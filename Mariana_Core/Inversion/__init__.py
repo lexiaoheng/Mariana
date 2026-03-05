411af8ad2fbf6fd8931eb09763257f0fd17a5f8c
Xiaoheng <351587229@qq.com>
2026-03-05 22:43:58 +0800

    Fix import conflict in __init__.py
    Removed conflicting import statement for DBDI.

--- Mariana_Core/Inversion/__init__.py
+++ Mariana_Core/Inversion/__init__.py
@@ -1,7 +1,4 @@
 from .encoder_inverter_framework.EIF import EIF
-<<<<<<< HEAD
 from .dual_branch_double_inversion.DBDI import DBDI
 from .physics_informed_acoustic_inversion.PIAI import PIAI
-=======
-from .dual_branch_double_inversion.DBDI import DBDI
->>>>>>> 7666eb5d9ae5a34ae7158fb5daa02bb8cfccb2c8
+
